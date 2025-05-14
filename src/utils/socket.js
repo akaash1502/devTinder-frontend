077602bc1312d3ec72ef6bf7996a9206753216c6
Akash Sharma <akashs3468@gmail.com>
2025-05-15 02:14:33 +0530

    ft-chat-prod

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,18 @@ import io from 'socket.io-client';
 import { BASE_URL } from './constants.js';
 
 export const createSocketConnection = () => {
-    return io(BASE_URL);
+    if(location.hostname === 'localhost') {
+        return io(BASE_URL);
+    } 
+
+    //ELSE CONDITION IS FOR PRODUCTION
+    else{
+        return io("/", {path: "/api/socket.io"});
+    }
+
+    //Calls happen on Socket.io
+    // but nginx doesnt know what to do with it
+    // Our backend runs on /api so we want BASE_URL to be /api for backend when on Production
+    // So we need to tell socket.io to use the same path
+
 };
